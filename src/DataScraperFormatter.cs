43dc407059852ade024b46a655e574b348dad15e
Nika Kolesnikova <kolesnikova.ve.0@gmail.com>
2025-06-20 08:52:47 -0400

    feat: Urls are being passed into the class instance as opposed to the Scrape method

--- src/DataScraperFormatter.cs
+++ src/DataScraperFormatter.cs
@@ -2,29 +2,33 @@ using System.Text.Json;
 
 public class DataScraperFormatter
 {
-    public required HttpClient client;
-    public async Task<List<string>> Scrape(List<string> urls)
+     public List<string> Urls { get; }
+    public DataScraperFormatter(List<string> urls)
+    {
+        Urls = urls;
+    }
+    public async Task<List<string>> Scrape()
     {
         using HttpClient client = new();
         List<string> returnData = new();
 
-        foreach (string url in urls)
+        foreach (string url in Urls)
         {
-             try
-                {
-                    // TODO use HtmlAgilityPack for HTML parsing
-                    var response = await client.GetAsync(url);
-                    response.EnsureSuccessStatusCode();
-                    var dataJson = await response.Content.ReadAsStringAsync();
-                    var doc = JsonDocument.Parse(dataJson);
-                    var prettified = JsonSerializer.Serialize(doc.RootElement, new JsonSerializerOptions { WriteIndented = true });
-                    returnData.Add(prettified);
-                }
-                catch (HttpRequestException ex)
-                {
-                    Console.WriteLine("Something went wrong with " + url + ". " + ex.Message);
-                    throw;
-                }
+            try
+            {
+                // TODO use HtmlAgilityPack for HTML parsing
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                var dataJson = await response.Content.ReadAsStringAsync();
+                var doc = JsonDocument.Parse(dataJson);
+                var prettified = JsonSerializer.Serialize(doc.RootElement, new JsonSerializerOptions { WriteIndented = true });
+                returnData.Add(prettified);
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("Something went wrong with " + url + ". " + ex.Message);
+                throw;
+            }
         }
 
         return returnData;
