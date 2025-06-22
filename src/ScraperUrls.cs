01a79feec455bcd2f5f618752ef1a4314974e82e
Nika Kolesnikova <kolesnikova.ve.0@gmail.com>
2025-06-22 09:46:36 -0400

    feat: Abstract urls to scrape into ScraperUrls class

---
+++ src/ScraperUrls.cs
@@ -0,0 +1,10 @@
+namespace FinanceNotifier.Src;
+
+public class ScraperUrls
+{
+    public static readonly Dictionary<string, string> FinanceUrls = new()
+    {
+        { "PGIM-Press-Release", "https://www.pgim.com/us/en/institutional/about-us/newsroom/press-releases"
+        }
+    };
+}
