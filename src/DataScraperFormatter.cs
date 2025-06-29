c0d03b24766206fa1e48bc74d287dae5dcafa88e
Nika Kolesnikova <kolesnikova.ve.0@gmail.com>
2025-06-29 12:08:14 -0400

    refactor(DataScraperFormatter): Implement scraping logic with Playwright

--- src/DataScraperFormatter.cs
+++ src/DataScraperFormatter.cs
@@ -1,9 +1,6 @@
-using System.Drawing;
-using Pastel;
-
 using System.Text.Json;
 
-using HtmlAgilityPack;
+using Microsoft.Playwright;
 
 namespace FinanceNotifier.Src;
 
@@ -20,8 +17,8 @@ public class DataScraperFormatter(Dictionary<string, string> urls)
             switch (url.Key)
             {
                 case "PGIM-Press-Release":
-                    ArticleData article = ScrapePGIMPressReleasePage(url.Value);
-                    scrapedArticles.Add(article);
+                    List<ArticleData> articles = await ScrapePGIMPressReleasePage(url.Value);
+                    scrapedArticles.AddRange(articles);
                     break;
                 default:
                     throw new Exception("Could not identify the urls");
@@ -31,43 +28,52 @@ public class DataScraperFormatter(Dictionary<string, string> urls)
         return scrapedArticles;
     }
 
-    public static ArticleData ScrapePGIMPressReleasePage(string url)
+    public async Task<List<ArticleData>> ScrapePGIMPressReleasePage(string url)
     {
         try
         {
+            IPage page = await GetNewPlaywrightPage();
             // Go to the page
-            var web = new HtmlWeb();
-            var htmlDoc = web.Load(url);
+            await page.GotoAsync(url);
 
-            // Look for anchors with a 'cmp-cta__link-wrapper' which should have "publishDate"
-            var anchors = htmlDoc.DocumentNode.SelectNodes("//a[contains(@class, 'cmp-cta__link-wrapper')]");
-            if (anchors == null || anchors.Count == 0)
+            // Wait for the content to load
+            await page.WaitForSelectorAsync(".cmp-search-list__item-group", new PageWaitForSelectorOptions
             {
-                throw new HighlightedException("Anchors are null");
+                State = WaitForSelectorState.Attached,
+            });
+
+            // Get list items
+            var listItems = await page.QuerySelectorAllAsync("li.cmp-searchresult-item");
+            if (listItems == null || listItems.Count == 0)
+            {
+                throw new HighlightedException("No articles found");
             }
 
-
-            // get its header and populate header into key
-            // get article and populate article into value
-            // push into common object
-
-            foreach (var node in anchors)
+            List<string> recentArticlesUrls = new();
+            // Loop over list items to identify recent articles
+            foreach (var li in listItems)
             {
-                // check type and date (published within the last week)
-                if (node.NodeType == HtmlNodeType.Element)
+                var anchor = await li.QuerySelectorAsync("a.cmp-searchresult-link");
+                // Get article's publish date
+                DateTime? datePublished = await GetPublishedDate(li);
+                if (anchor != null && datePublished != null && IsPublishedWithinLastWeek(datePublished.Value))
                 {
-                    DateTime? datePublished = GetPublishedDate(node);
-                    if (datePublished != null && IsPublishedWithinLastWeek(datePublished.Value))
-                    {
-                        // click on that one
-                        string href = node.GetAttributeValue("href", "");
-                        Console.WriteLine("href: ", href);
-                    }
+                    // Click on a recent article
+                    string href = await anchor.GetAttributeAsync("href") ?? string.Empty;
+                    recentArticlesUrls.Add(href);
                 }
             }
-
-            var pgimArticle = new ArticleData();
-            return pgimArticle;
+            if (recentArticlesUrls.Count == 0)
+            {
+                Console.WriteLine("No articles within the requested timeframe.");
+            }
+            List<ArticleData> recentPgimArticles = new();
+            foreach (var articleUrl in recentArticlesUrls)
+            {
+                ArticleData article = await GetPgimArticleData(articleUrl);
+                recentPgimArticles.Add(article);
+            }
+            return recentPgimArticles;
         }
         catch (HttpRequestException ex)
         {
@@ -76,34 +82,19 @@ public class DataScraperFormatter(Dictionary<string, string> urls)
         }
     }
 
-    public static DateTime? GetPublishedDate(HtmlNode node)
+    public async Task<DateTime?> GetPublishedDate(IElementHandle node)
     {
-        // Extract 'data-cmp-data-layer' attribute
-        string dataCmpDataLayerAttribute = node.Attributes["data-cmp-data-layer"].Value
-        ?? throw new HighlightedException("Could not find 'data-cmp-data-layer' attribute");
-
-        // Decode HTML entities
-        string decodedData = System.Net.WebUtility.HtmlDecode(dataCmpDataLayerAttribute);
-
         try
         {
-            // Parse the decoded string using System.Text.Json
-            var parsedDoc = JsonDocument.Parse(decodedData);
-            // Extract the publishDate field
-            var root = parsedDoc.RootElement;
-            foreach (var property in root.EnumerateObject())
+            IElementHandle datePublishedElement = await node.QuerySelectorAsync("span.cmp-searchresult-date")
+        ?? throw new HighlightedException("Could not find 'cmp-searchresult-date' span");
+            string datePublishedString = await datePublishedElement.InnerTextAsync();
+
+            if (!DateTime.TryParse(datePublishedString, out DateTime publishDate))
             {
-                var entry = property.Value;
-
-                string publishDateString = entry.GetProperty("publishDate").GetString()
-                ?? throw new HighlightedException("Could not get publishDate");
-
-                if (!DateTime.TryParse(publishDateString, out DateTime publishDate))
-                {
-                    throw new HighlightedException($"Failed to parse publishDate: {publishDateString}");
-                }
-                return publishDate;
+                throw new HighlightedException($"Failed to parse publishDate: {datePublishedString}");
             }
+            return publishDate;
         }
         catch (JsonException ex)
         {
@@ -113,8 +104,29 @@ public class DataScraperFormatter(Dictionary<string, string> urls)
         return null;
     }
 
-    public static bool IsPublishedWithinLastWeek(DateTime publishDate)
+    public bool IsPublishedWithinLastWeek(DateTime publishDate)
     {
         return (DateTime.Today - publishDate).TotalDays < 7;
+    }
+
+    public async Task<ArticleData> GetPgimArticleData(string articleUrl)
+    {
+        Console.WriteLine($"{articleUrl}");
+        ArticleData article = new();
+        IPage page = await GetNewPlaywrightPage();
+        await page.GotoAsync(articleUrl);
+
+        return article;
+    }
+
+    public async Task<IPage> GetNewPlaywrightPage()
+    {
+        // Initialize Playwright
+        var playwright = await Playwright.CreateAsync();
+        // Initialize a browser and a new page
+        var browser = await playwright.Chromium.LaunchAsync(
+        new BrowserTypeLaunchOptions { Headless = true });
+
+        return await browser.NewPageAsync();
     }
 }
