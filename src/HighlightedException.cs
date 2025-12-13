104dfb791772bcccba0e9afece9c418a8b0b294d
Nika Kolesnikova <kolesnikova.ve.0@gmail.com>
2025-12-13 15:51:40 -0500

    style(HighlightedException): Use red color for consistency

--- src/HighlightedException.cs
+++ src/HighlightedException.cs
@@ -7,5 +7,5 @@ namespace FinanceNotifier.Src;
 public class HighlightedException : Exception
 {
     public HighlightedException(string message)
-        : base(message.Pastel(Color.OrangeRed)) { }
+        : base(message.Pastel(Color.Red)) { }
 }
